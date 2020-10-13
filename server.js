e4d2aae82d2fa1d65be93105c40fcae8ff850c30
Chidinma Nze <chidinma.ogbuaku@gmail.com>
2020-10-13 00:40:55 -0400

    files

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+var db = require("./models");
+
+app.use(logger("dev"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {
+  useNewUrlParser: true
+});
+
+app.listen(PORT, function() {
+  console.log(`Now listening on port: ${PORT}`);
+});
